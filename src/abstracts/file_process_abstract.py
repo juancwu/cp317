38a48de0bfd23e92f790aee97bd3c38554b6e46c
Juan Wu <46619361+juancwu@users.noreply.github.com>
2022-11-22 13:19:17 -0500

    changing project structure

--- src/abstracts/file_process_abstract.py
+++ src/abstracts/file_process_abstract.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractmethod
-
+from io import TextIOWrapper
 """
 Abstract class for file processor
 It has two methods: open_file and close_file
 open_file: given a file path, it opens the file and returns the file object
 close_file: given a file object, it closes the file
 """
-class FileProcessor(ABC):
+class FileProcessorAbstract(ABC):
     @abstractmethod
-    def open_file(self, file_path):
+    def open_file(self, file_path: str) -> TextIOWrapper or None:
         pass
 
     @abstractmethod
-    def close_file(self, file_object):
+    def close_file(self, file_object: TextIOWrapper) -> None:
         pass
