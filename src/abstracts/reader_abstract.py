9f5f79e542fcfaa4e2be0124bb5ebfef376794f1
Geun Han Chung <a2782541671@gmail.com>
2022-11-21 20:09:26 -0500

    writer and reader abstract made

---
+++ src/abstracts/reader_abstract.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+"""
+Abstract class for reader class
+It has three methods: open_file and close_file and read
+open_file: given a file path, it opens the file and returns the file object, override from file_process class
+close_file: given a file object, it closes the file, override from file_process class
+read: given a file, we can read the file and use formatter to format the string inside the file given
+"""
+class Reader(ABC):
+    @abstractmethod
+    def read(self, file_path):
+        pass
+
+    @abstractmethod
+    def open_file(self, file_path):
+        pass
+
+    @abstractmethod
+    def close_file(self, file_object):
+        pass
+    
