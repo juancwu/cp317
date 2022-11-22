49ccc639cd6744cfa8354a2ce7624ed8a792e5c1
juancwu <jc@macbook.com>
2022-11-21 19:55:06 -0500

    finished the abstraction for formatter class

---
+++ src/abstracts/formatter_abstract.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+"""
+Abstract class for formatter
+"""
+
+class FormatterAbstract(ABC):
+    @abstractmethod
+    def format(self, data):
+        """
+        Output data in a specific format:
+        id, name, course code, final grade
+        """
+        pass
+
+    @abstractmethod
+    def load_name_data(self, data):
+        """
+        Load name data into the formatter
+        stored in the format of:
+        id, name
+        """
+        pass
+
+    @abstractmethod
+    def load_course_data(self, data):
+        """
+        Load course data into the formatter
+        stored in the format of:
+        id, course code, test 1, test 2, test 3, final exam
+        """
+        pass
